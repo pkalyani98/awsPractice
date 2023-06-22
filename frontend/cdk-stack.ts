2b47f615419abf198f5c7d15b44c5c56c4dc6754
pkalyani98 <pkalyani@greenactionstudio.com>
2023-06-22 11:09:50 -0400

    working frontend

--- frontend/cdk-stack.ts
+++ frontend/cdk-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+// import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
 import { Construct } from 'constructs';
+import { BlockPublicAccess, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 // import * as iam from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -15,21 +16,22 @@ export class FrontEndStack extends cdk.Stack {
       autoDeleteObjects: true,
       publicReadAccess: true,
       websiteIndexDocument: 'index.html',
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
     });
     new s3deploy.BucketDeployment(this, `myreactapp-s3bucketdeployment`, {
       sources: [s3deploy.Source.asset('./build')],
       destinationBucket: bucket,
-      destinationKeyPrefix: 'build',
     });
-    new cloudFront.CloudFrontWebDistribution(this, 'CDKCRAStaticDistribution', {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: bucket,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-    });
+    // new cloudFront.CloudFrontWebDistribution(this, 'CDKCRAStaticDistribution', {
+    //   originConfigs: [
+    //     {
+    //       s3OriginSource: {
+    //         s3BucketSource: bucket,
+    //       },
+    //       behaviors: [{ isDefaultBehavior: true }],
+    //     },
+    //   ],
+    // });
   }
 }
