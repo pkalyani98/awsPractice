10434072f5396834d3ab88950e0b7833545a0fb1
pkalyani98 <pkalyani@greenactionstudio.com>
2023-06-22 11:35:40 -0400

    removed comments

--- backend/lib/cdk-stack.ts
+++ backend/lib/cdk-stack.ts
@@ -11,24 +11,6 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const myTopic = new sns.Topic(this, 'MyTopic');
-    // const bucket = new s3.Bucket(this, "CdkTestBucket",{
-    //   publicReadAccess:true,
-    //   websiteIndexDocument:"index.html"
-    // })
-    // new s3deploy.BucketDeployment(this, `myreactapp-s3bucketdeployment`, {
-    //   sources: [s3deploy.Source.asset("../build")],
-    //   destinationBucket: bucket,
-    // });
-    // const cf = new cloudFront.CloudFrontWebDistribution(this, "CDKCRAStaticDistribution", {
-    //   originConfigs: [
-    //     {
-    //       s3OriginSource: {
-    //         s3BucketSource: bucket
-    //       },
-    //       behaviors: [{isDefaultBehavior: true}]
-    //     },
-    //   ]
-    // });
     const api = new ApiGateway(this);
     const postLambda = new Lambda(this, 'post', myTopic);
     myTopic.addSubscription(new subscriptions.EmailSubscription('pkalyani@buffalo.edu'));
